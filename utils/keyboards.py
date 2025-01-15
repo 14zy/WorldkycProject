d0c375bcbcbf3effb73b862bf75bac3f627dcac2
Ruben <just14zy@gmail.com>
2025-01-15 16:25:06 +0700

    new login

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,5 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 
 web = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Login", web_app=WebAppInfo(url="https://app.worldkyc.com/login"))]
+    [InlineKeyboardButton(text="Login", web_app=WebAppInfo(url="https://t.me/worldkycbot/app"))]
 ])
