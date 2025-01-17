90c4264758f3001d3af833d4e1f646b0da985a33
plasticlife-art <plastic.life.ln@gmail.com>
2025-01-17 11:29:30 +0100

    change end property name

--- config/config.py
+++ config/config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = os.getenv("API_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 AUTHORIZED_TOKEN = os.getenv("AUTHORIZED_TOKEN")
 
 url_webapp = "https://t.me/worldkycbot"
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=BOT_TOKEN)
